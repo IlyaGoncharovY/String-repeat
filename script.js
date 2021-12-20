4cdd5eb5ae74beee5a6fa6bb320e791071f66bbc
IlyaGoncharovY <goncharov.i@rambler.ru>
2021-12-20 22:21:20 +0300

    test ok

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-function repeatStr (n, s) {
-    // Write a function called repeatStr which 
-    // repeats the given string
-    //  string exactly n times.
+function repeatStr(count, str) {
+    let repeatedString = "";
+    for (i = 0; i < count; i++) {
+        repeatedString += str;
+    }
+    return repeatedString;
 }
